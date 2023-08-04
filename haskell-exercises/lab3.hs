da1c931e1f9cbf0f1ba3552981e6dbfe5a28a1ea
basedtheorem <lrns@proton.me>
2023-08-05 00:19:03 +1200

    feat(hs): complete lab3

--- haskell-exercises/lab3.hs
+++ haskell-exercises/lab3.hs
@@ -42,3 +42,32 @@ markPow2 ls = [(x,isPow2 x) | x <- ls]
 
 
 -- ——3.3——
+ints :: [Int]
+ints = [3, 32, 0, 4, -90]
+
+floats :: [Float]
+floats = [3.9, 32.1, 0.00001, 0, -99.9999]
+
+doubles :: [Double]
+doubles = [3.33333333333333333, 3, 7, -30.3, -9]
+
+
+-- ——3.4——
+-- 3.4.1
+len :: Ord a => [a] -> Int
+len []    = 0
+len (_:t) = 1 + len t
+
+-- 3.4.2
+mean :: Real a => [a] -> Fractional b => b
+mean ls = realToFrac (sum ls) / fromIntegral (len ls)
+
+var :: Real a => [a] -> Fractional b => b
+var ls =
+  let avg = mean ls
+  in (sum [(realToFrac x - avg)^2 | x <- ls])
+      /
+     fromIntegral (len ls)
+
+stdev :: Real a => [a] -> Floating b => b
+stdev ls = sqrt (var ls)
