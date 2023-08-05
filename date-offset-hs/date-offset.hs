6131a382c50f75812e46956779b8f3d9b776c39d
basedtheorem <lrns@proton.me>
2023-08-05 16:11:41 +1200

    refactor(date-offset): rename & shorten vars

--- date-offset-hs/date-offset.hs
+++ date-offset-hs/date-offset.hs
@@ -1,6 +1,6 @@
 adjustDate :: (Int, Int, Int) -> Int -> (Int, Int, Int)
 adjustDate (day, month, year) offset =
-    if checkValidDate (day, month, year)
+    if isDateValid (day, month, year)
     then addDaysToDate (day, month, year) offset
     else error ""
 
@@ -18,14 +18,16 @@ normaliseDate :: Int -> Int -> (Int, Int)
 normaliseDate totDays prevYr
     | totDays <= 365 && totDays >= 1 = (totDays, prevYr)
     | totDays == 366 && isLeapYear prevYr = (totDays,prevYr)
-    | totDays == 0 = (if isLeapYear (prevYr - 1) then 366 else 365,prevYr - 1)
+    | totDays == 0 =
+        (if isLeapYear (prevYr - 1) then 366 else 365, prevYr - 1)
     | otherwise =
         let
             finalYr = prevYr +
                 if totDays < 0
                 then ceiling (fromIntegral totDays / 365) - 1
                 else floor (fromIntegral totDays / 365)
-            finalDays = (totDays `mod` 365) - countLeapsInInterval prevYr finalYr
+            finalDays = (totDays `mod` 365) -
+                countLeapsBetween prevYr finalYr
         in
             if totDays < 1
             then normaliseDate totDays finalYr
@@ -40,21 +42,21 @@ normaliseDate totDays prevYr
 --         then normaliseDate finalDays (finalYr + 1)
 --         else (finalDays, finalYr)
 
-countLeapsInInterval :: Int -> Int -> Int
-countLeapsInInterval yr1 yr2 = sum [1 | y <- [yr1 .. yr2], isLeapYear y]
+countLeapsBetween :: Int -> Int -> Int
+countLeapsBetween yr1 yr2 = sum [1 | y <- [yr1 .. yr2], isLeapYear y]
 
 -- #TODO
-checkValidDate :: (Int, Int, Int) -> Bool
-checkValidDate (date, month, year) = True
+isDateValid :: (Int, Int, Int) -> Bool
+isDateValid (date, month, year) = True
 
 daysOfYearToDate :: Int -> Int -> (Int, Int, Int)
 daysOfYearToDate totalDays yr =
     let
-        cMonths = cumulativeMonths yr
-        month = getFirstIndexGreaterOrEqThan totalDays cMonths
-        day = totalDays - valueAt (month - 1) cMonths
+        cMon = cumulativeMonths yr
+        mon = getFirstGeqIndex totalDays cMon
+        day = totalDays - valueAt (mon - 1) cMon
     in
-        (day, month, yr)
+        (day, mon, yr)
 
 dateToDaysOfYear :: (Int, Int, Int) -> Int
 dateToDaysOfYear (d, m, y) = d + getCumulativeDaysOfMonth m y
@@ -63,7 +65,7 @@ isLeapYear :: Int -> Bool
 isLeapYear year =
     year `mod` 4 == 0 && (
     year `mod` 100 /= 0 ||
-    year `mod` 400 == 0    )
+    year `mod` 400 == 0  )
 
 valueAt :: Ord a => Int -> [a] -> a
 valueAt index (h:t)
@@ -72,21 +74,20 @@ valueAt index (h:t)
 
 getCumulativeDaysOfMonth :: Int -> Int -> Int
 getCumulativeDaysOfMonth month yr =
-    valueAt    (month - 1) (cumulativeMonths yr)
+    valueAt (month - 1) (cumulativeMonths yr)
 
 cumulativeMonths :: Int -> [Int]
 cumulativeMonths year
     | isLeapYear year = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
-    | otherwise             = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+    | otherwise       = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
 
-getFirstIndexGreaterOrEqThan :: Int -> [Int] -> Int
-getFirstIndexGreaterOrEqThan _ [] = -1
-getFirstIndexGreaterOrEqThan num (h : t)
-    | h >= num = 0 -- return *this* index
-    | current == -1 = -1 -- no elem greater than
-    | otherwise = 1 + current -- increment index
-    where
-        current = getFirstIndexGreaterOrEqThan num t
+getFirstGeqIndex :: Int -> [Int] -> Int
+getFirstGeqIndex _ [] = -1
+getFirstGeqIndex num (h : t)
+    | h >= num = 0
+    | current == -1 = -1
+    | otherwise = 1 + current
+      where current = getFirstGeqIndex num t
 
 -- ———— Unused ———— --
 daysInMonth :: Int -> Bool -> Int
